7022f813af1bc38580f474e3c5f80a6d40e98dba
Chris Mingay <chris@chrismingay.co.uk>
2022-06-13 16:53:50 +0100

    Remove old sample code

--- src/WebBlazor/Program.cs
+++ src/WebBlazor/Program.cs
@@ -1,8 +1,5 @@
 using Application;
 using Infrastructure;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using WebBlazor.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +9,6 @@ builder.Services.AddInfrastructure(builder.Configuration);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
